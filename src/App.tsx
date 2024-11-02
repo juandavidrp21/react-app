93d366d4b0e7e3ff4713648312b86579b106a15c
juandavidrp21 <juandavidrp21@gmail.com>
2024-11-01 23:38:04 -0500

    cambios 11-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+import button from "./components/button";
 
 function App() {
-  const list = ["Juanda", "Tanjiro", "Eren"];
+  const list: string[] = ["Juanda", "Tanjiro", "Eren"];
+
+  const handleSelect = (elemento: String) => {
+    console.log(elemento);
+  };
+
+  const containerList = list.length ? (
+    <List data={list} onSelect={handleSelect} />
+  ) : (
+    "Sin valores para mostrar"
+  );
+
+  const [btnCarga, setbtnCarga] = useState(false);
 
   return (
     <Card>
       <CardBody tittle="Este es el Título" text="Este es el tex" />
-      <List data={list} />
+      {containerList}
+
+      {!btnCarga ? (
+        <button
+          onClick={() => setbtnCarga(true)}
+          type="button"
+          className="btn btn-primary"
+        >
+          Guardar
+        </button>
+      ) : (
+        <button
+          onClick={() => setbtnCarga(false)}
+          type="button"
+          className="btn btn-secondary"
+        >
+          Cargando...
+        </button>
+      )}
     </Card>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
+
 type Props = {
   data: string[];
+  onSelect?: (elemento: string) => void;
 };
 
-function List({ data }: Props) {
-  const handleClick = (e: string) => {
-    console.log(e);
+function List({ data, onSelect }: Props) {
+  const [index, setIndex] = useState(1);
+  const handleClick = (i: number, elemento: string) => {
+    setIndex(i);
+    onSelect?.(elemento);
   };
 
   return (
     <ul className="list-group">
-      {data.map((elemento) => (
+      {data.map((elemento, i) => (
         <li
-          onClick={() => handleClick(elemento)}
+          onClick={() => handleClick(i, elemento)}
           key={elemento}
-          className="list-group-item"
+          className={`list-group-item ${index == i ? "active" : ""}`}
         >
           {elemento}
         </li>
